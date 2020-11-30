acbd17d2b2605956893848721890474adf3d7e6d
Max Block <maxblock@pm.me>
2020-11-30 09:28:25 +0300

    update

---
+++ test/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>It works</div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {}
+}
+</script>
+
