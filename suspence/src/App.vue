00619f381a51ef71920206b76a15e4389452e97f
Max Block <maxblock@pm.me>
2020-12-17 10:51:14 +0300

    update

---
+++ suspence/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <suspense>
+    <template #default>
+      <AsyncData />
+    </template>
+    <template #fallback>
+      Loading...
+    </template>
+  </suspense>
+</template>
+
+<script>
+import AsyncData from './components/AsyncData.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AsyncData
+  }
+}
+</script>
+
+
---
+++ suspence/src/components/AsyncData.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul>
+    <li v-for="(n, index) in numbers" :key="index">{{ n }}</li>
+  </ul>
+</template>
+
+<script>
+import {ref} from "@vue/reactivity";
+
+export default {
+  async setup() {
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    const numbers = ref([1, 2, 3, 4, 5])
+    return {numbers}
+  }
+}
+</script>
